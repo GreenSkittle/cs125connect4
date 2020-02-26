cafd7858b04bd97eb758f219a27377694e2b2596
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-02-25 19:46:40 -0700

    Update and rename connect4-main.c to connect4_main.c
    lay out prototypes and function order

---
+++ connect4_main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <math.h>
+#include <time.h>
+//
+#define
+#define
+#define
+int main(){
+    resetDisplay();
+    
+    testforwin();
+    promptUser();
+    checkValidity();
+    modBoard();
+    drawBoard();
+    testforwin();
+    
+    return 0;
+}
