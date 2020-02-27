42b0e4cda5c9d8a22ec76fa2c4b431c1232cdef6
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-02-27 14:12:02 -0700

    Update drawmodBoard.c

--- drawmodBoard.c
+++ drawmodBoard.c
@@ -3,7 +3,7 @@
 // makes use of the global artboard variable
 // artboard is a pointer to the ascii art -  defined globally
 // ok now the artboard is unique to this function
-void drawBoard(int boardmatrix[WIDTH][HEIGHT], column, player){
+void drawmodBoard(int boardmatrix[WIDTH][HEIGHT], column, player){
     int j,k;
     for (j=0;j<=HEIGHT;j++){
         for (k=0;k<=WIDTH;k++){
