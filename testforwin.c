61b7f0a4000d0a201bd212849098e4e625af0978
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-02-27 14:18:48 -0700

    Create testforwin.c

---
+++ testforwin.c
@@ -0,0 +1,8 @@
+// TITLE BLOCK
+int testforwin(int boardmatrix[HEIGHT][WIDTH]){
+    int resultant = -1;
+    // legend: -1: no data or game is in progress, 
+    // 0: player 0 wins, 1: player 1 wins, 2: tie!
+
+    return resultant;
+}
