e53dd7d6634b21e11209c443547ba57414b23de8
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-02-27 14:11:45 -0700

    Update and rename drawBoard.c to drawmodBoard.c
    made artboard unique to function

--- drawmodBoard.c
+++ drawmodBoard.c
@@ -2,7 +2,8 @@
 // below is the function that must match the main file, including definitions
 // makes use of the global artboard variable
 // artboard is a pointer to the ascii art -  defined globally
-void drawBoard(int boardmatrix[WIDTH][HEIGHT], char artboard[][]){
+// ok now the artboard is unique to this function
+void drawBoard(int boardmatrix[WIDTH][HEIGHT], column, player){
     int j,k;
     for (j=0;j<=HEIGHT;j++){
         for (k=0;k<=WIDTH;k++){
