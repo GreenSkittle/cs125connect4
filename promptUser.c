f2999b16f794ee165c4478bc55fe449a7a1497d3
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-03-03 14:30:22 -0700

    Update promptUser.c

--- promptUser.c
+++ promptUser.c
@@ -1,18 +1,33 @@
 // TITLE BLOCK
-//prototype?
-
+#define COLUMNS 7
 int promptUser(int player){
 char choice;
-
-printf("\033[1;31m");
-printf("Player 1 ");
-printf("\033[0m");
-printf("what column do you choose?\n");
-scanf(%c,&choice);
-printf("\033[0;33m");
-printf("Player 2 ");
-printf("\033[0m");
-printf("what column do you choose?\n");
-scanf(%c,&choice);
-return ?;
+int location;
+switch (player){
+	case 1:
+		printf("\033[1;31m");
+		printf("\nPlayer 1: ");
+		printf("\033[0m");
+		printf("Pick a spot A-G:\n");
+		scanf("%c",&choice);
+		break;
+	case 2:
+		printf("\033[1;31m");
+		printf("\nPlayer 2: ");
+		printf("\033[0m");
+		printf("Pick a spot A-G:\n");
+		scanf("%c",&choice);
+		break;
+	default: break;
+}
+if(location >= 65 && location <= 90){
+	location = (int) choice - 65;
+}
+else if(location >= 97 && location <= 122){
+	location = (int) choice - 97;
+}
+else {
+	location = -1;
+}
+return location;
 }
