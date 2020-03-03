45a655b82e417e453212137428bf4f74512b9728
mgmaclean00 <61476975+mgmaclean00@users.noreply.github.com>
2020-03-03 14:16:53 -0700

    Update promptUser.c

--- promptUser.c
+++ promptUser.c
@@ -1,6 +1,18 @@
 // TITLE BLOCK
 //prototype?
-int promptUser(int player){
 
+int promptUser(int player){
+char choice;
+
+printf("\033[1;31m");
+printf("Player 1 ");
+printf("\033[0m");
+printf("what column do you choose?\n");
+scanf(%c,&choice);
+printf("\033[0;33m");
+printf("Player 2 ");
+printf("\033[0m");
+printf("what column do you choose?\n");
+scanf(%c,&choice);
 return ?;
 }
