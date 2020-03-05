94e790007657dcde7fa9bb4b3b8653c33f4da986
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-03-05 14:11:51 -0700

    Update promptUser.c

--- promptUser.c
+++ promptUser.c
@@ -1,33 +1,35 @@
-// TITLE BLOCK
-#define COLUMNS 7
+//
 int promptUser(int player){
-char choice;
-int location;
-switch (player){
-	case 1:
-		printf("\033[1;31m");
-		printf("\nPlayer 1: ");
-		printf("\033[0m");
-		printf("Pick a spot A-G:\n");
-		scanf("%c",&choice);
-		break;
-	case 2:
-		printf("\033[1;31m");
-		printf("\nPlayer 2: ");
-		printf("\033[0m");
-		printf("Pick a spot A-G:\n");
-		scanf("%c",&choice);
-		break;
-	default: break;
-}
-if(location >= 65 && location <= 90){
-	location = (int) choice - 65;
-}
-else if(location >= 97 && location <= 122){
-	location = (int) choice - 97;
-}
-else {
-	location = -1;
-}
-return location;
+	char choice;
+	int location;
+	int quickcheck;
+	switch (player){
+		case 1:
+			printf("\033[1;31m");
+			printf("\nPlayer 1: ");
+			printf("\033[0m");
+			printf("Pick a spot A-G:\n");
+			quickcheck = scanf("%c",choice);
+			break;
+		case 2:
+			printf("\033[1;31m");
+			printf("\nPlayer 2: ");
+			printf("\033[0m");
+			printf("Pick a spot A-G:\n");
+			quickcheck = scanf("%c",choice);
+			break;
+		default: break;
+	}
+	// CAPITALS A-Z
+	if(choice >= 65 && choice <= 90 && quickcheck == 1){
+		location = choice - 65;
+	}
+	// LOWERCASE a-z
+	else if(choice >= 97 && choice <= 122 && quickcheck == 1){
+		location = choice - 97;
+	}
+	else {
+		location = -1;
+	}
+	return location;
 }
