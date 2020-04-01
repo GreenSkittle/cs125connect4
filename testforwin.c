1e066252417c248f222a5f7871f13211560167a6
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-03-31 19:56:03 -0700

    Update testforwin.c

--- testforwin.c
+++ testforwin.c
@@ -1,8 +1,8 @@
-// TITLE BLOCK
-int testforwin(int boardmatrix[HEIGHT][WIDTH]){
-    int resultant = -1;
+int testforwin(int* pointy){
+    int result = -1;
     // legend: -1: no data or game is in progress, 
     // 0: player 0 wins, 1: player 1 wins, 2: tie!
 
-    return resultant;
+    return result;
 }
+/* the algorithm should detect when 5 adjacent or diagonal cells are the same value!*/
