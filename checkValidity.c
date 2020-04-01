edc1674d055aad7af4ce8ec36791c298549a2856
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-03-31 19:52:27 -0700

    Update checkValidity.c

--- checkValidity.c
+++ checkValidity.c
@@ -2,7 +2,7 @@
 // if the player is able to go in that location (0-6) return a 1
 // if the location is invalid, such as -1,
 // or the column is filled to the top, return a zero!
-int checkValidity(boardptr, column){
+int checkValidity(int* pointy, column){
 
 return result;
 }
