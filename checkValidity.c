f111e623e98f3fe9c1e110485253f2042384565a
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-03-31 19:51:39 -0700

    Update checkValidity.c

--- checkValidity.c
+++ checkValidity.c
@@ -1,10 +1,8 @@
-//prototype
-// write a function that receives the board, or a pointer to the board,
-// as well as a column
+// write a function that receives a pointer to the board, a location.
 // if the player is able to go in that location (0-6) return a 1
 // if the location is invalid, such as -1,
-// or the column is filled to the top, return a zero.
-int checkValidity(int boardmatrix[ROWS][COLUMNS], column){
+// or the column is filled to the top, return a zero!
+int checkValidity(boardptr, column){
 
-return resultant;
+return result;
 }
