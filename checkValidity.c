7ff7dea99c2d8e3e7705e4e64ec30dbf82194cbe
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-04 21:23:50 -0700

    Update checkValidity.c
    no longer pass in pointer, just whole board.

--- checkValidity.c
+++ checkValidity.c
@@ -1,8 +1,7 @@
-// write a function that receives a pointer to the board, a location.
-// if the player is able to go in that location (0-6) return a 1
-// if the location is invalid, such as -1,
-// or the column is filled to the top, return a zero!
-int checkValidity(int* pointy, column){
-
-return result;
+int checkValidity(int wholeboard[ROWS][COLUMNS], int column){
+    int result=0;
+    if (column >= 0 && column < COLUMNS && wholeboard[0][column] == 0){
+        result = 1;
+    }
+    return result;
 }
