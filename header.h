4db09a7ebabec7d89759c28d64f6e2b2535e188d
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-06 15:02:12 -0700

    Update header.h

--- header.h
+++ header.h
@@ -1,14 +1,14 @@
 #ifndef HEADER_H
 #define HEADER_H
-#define ROWS 6
-#define COLUMNS 7
-// DEMO STRUCTURE
-typedef struct game {
-    int turnNumber;
-    char board[ROWS][COLUMNS];
-    int winState;
-} Game;
-// FUNCTION PROTOTYPES
+    #define ROWS 6
+    #define COLUMNS 7
+    // DEMO STRUCTURE
+    typedef struct game {
+        int turnNumber;
+        char board[ROWS][COLUMNS];
+        int winState;
+    } Game;
+    // FUNCTION PROTOTYPES
     void drawmodBoard(int board[ROWS][COLUMNS], int column, int player);
     void resetDisplay(void);
     int testforwin(int board[ROWS][COLUMNS);
