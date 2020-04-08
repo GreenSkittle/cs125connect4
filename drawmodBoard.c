9df3841fbd94b016f2b22a92f2b926c98133a952
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-08 00:06:45 -0700

    Update drawmodBoard.c

--- drawmodBoard.c
+++ drawmodBoard.c
@@ -1,6 +1,6 @@
 #include "header.h"
 void drawmodBoard(int boardmatrix[ROWS][COLUMNS], int column, int player){
-    int k, j;
+    int k;
     if (column !=-1 && player !=-1){
         for (k = ROWS-1; k >= 0; k--){
             if (boardmatrix[k][column] == 0){
