ee97af448fea5b25d5d603dda4670e7e1293dd91
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-08 14:45:09 -0700

    comments

--- checkValidity.c
+++ checkValidity.c
@@ -2,6 +2,7 @@
 int checkValidity(int wholeboard[ROWS][COLUMNS], int column){
     int result = 0;
     if (column >= 0 && column < COLUMNS && wholeboard[0][column] == 0){
+        // Ensure the chosen place exists and is open
         result = 1;
     }
     return result;
