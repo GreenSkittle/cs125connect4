f07b0afcca81391c0669d22e3398799085845e03
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-08 00:57:42 -0700

    first version
    probably going to rewrite this. the padding confuses the algorithm

--- testforwin.c
+++ testforwin.c
@@ -1,8 +1,65 @@
-int testforwin(int* pointy){
-    int result = -1;
-    // legend: -1: no data or game is in progress, 
-    // 0: player 0 wins, 1: player 1 wins, 2: tie!
-
+#include "header.h"
+int testforwin(int board[ROWS][COLUMNS]){
+    int result, j, k, fullboard = 0;
+    int padboard[ROWS+8][COLUMNS+8] = {
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    };
+    for (k = 0; k <= COLUMNS - 1; k++){
+        if (board[0][k] == 0){
+            break;
+        }
+        else {
+            fullboard = 1;
+        }
+    }
+    for (j = 0; j <= ROWS - 1; j++){
+        for (k = 0; k <= COLUMNS - 1; k++){
+            padboard[j+4][k+4] = board[j][k];
+        }
+    }
+    for (j = 0; j <= ROWS - 1; j++){
+        for (k = 0; k <= COLUMNS - 1; k++){
+            if (padboard[k+4][j+4] == 1 && padboard[k+4][j+5] == 1 && padboard[k+4][j+6] == 1 && padboard[k+4][j+7] == 1){
+                result = 1;
+            }
+            else if (padboard[k+4][j+4] == 2 && padboard[k+4][j+5] == 2 && padboard[k+4][j+6] == 2 && padboard[k+4][j+7] == 2){
+                result = 2;
+            }
+            else if (padboard[k+4][j+4] == 1 && padboard[k+5][j+4] == 1 && padboard[k+6][j+4] == 1 && padboard[k+7][j+4] == 1){
+                result = 1;
+            }
+            else if (padboard[k+4][j+4] == 2 && padboard[k+5][j+4] == 2 && padboard[k+6][j+4] == 2 && padboard[k+7][j+4] == 2){
+                result = 2;
+            }
+            else if (padboard[k+4][j+4] == 1 && padboard[k+5][j+5] == 1 && padboard[k+6][j+6] == 1 && padboard[k+7][j+7] == 1){
+                result = 1;
+            }
+            else if (padboard[k+4][j+4] == 2 && padboard[k+5][j+5] == 2 && padboard[k+6][j+6] == 2 && padboard[k+7][j+7] == 2){
+                result = 2;
+            }
+            else if (padboard[k+4][j+4] == 1 && padboard[k+3][j+5] == 1 && padboard[k+2][j+6] == 1 && padboard[k+1][j+7] == 1){
+                result = 1;
+            }
+            else if (padboard[k+4][j+4] == 2 && padboard[k+3][j+5] == 2 && padboard[k+2][j+6] == 2 && padboard[k+1][j+7] == 2){
+                result = 2;
+            }
+            else if (fullboard == 1){
+                result = 3;
+            }
+        }
+    }
     return result;
 }
-/* the algorithm should detect when 5 adjacent or diagonal cells are the same value!*/
