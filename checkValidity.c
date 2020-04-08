3f3fb4eb50f5812f2d1ef98348ff8633c06ddd1d
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-08 00:58:48 -0700

    Update checkValidity.c

--- checkValidity.c
+++ checkValidity.c
@@ -1,6 +1,6 @@
 #include "header.h"
 int checkValidity(int wholeboard[ROWS][COLUMNS], int column){
-    int result=0;
+    int result = 0;
     if (column >= 0 && column < COLUMNS && wholeboard[0][column] == 0){
         result = 1;
     }
