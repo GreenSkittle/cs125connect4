9af47d6983598649d5482bc1583bf574e2dee050
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-08 01:03:03 -0700

    Update drawmodBoard.c
    primitive printing actually works

--- drawmodBoard.c
+++ drawmodBoard.c
@@ -1,8 +1,8 @@
 #include "header.h"
 void drawmodBoard(int boardmatrix[ROWS][COLUMNS], int column, int player){
-    int k;
-    if (column !=-1 && player !=-1){
-        for (k = ROWS-1; k >= 0; k--){
+    int k, j;
+    if (column != -1 && player != -1){
+        for (k = ROWS - 1; k >= 0; k--){
             if (boardmatrix[k][column] == 0){
                 boardmatrix[k][column] = player;
                 break;
@@ -10,8 +10,11 @@ void drawmodBoard(int boardmatrix[ROWS][COLUMNS], int column, int player){
         }
     }
     // PRIMITIVE PRINTING
-    for (k = 0; k <= ROWS-1; k++){
-        printf("\n%s",boardmatrix[k]);
+    for (k = 0; k <= ROWS - 1; k++){
+        printf("\n");
+        for (j = 0; j <= COLUMNS - 1; j++){
+            printf(" %d",boardmatrix[k][j]);
+        }
     }
     printf("\n");
 }
