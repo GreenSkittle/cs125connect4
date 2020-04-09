0648453d7379eea7639a001b4c7cbd0992698e51
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-09 00:47:49 -0700

    Update drawmodBoard.c
    forgot break statements

--- drawmodBoard.c
+++ drawmodBoard.c
@@ -1,7 +1,6 @@
 #include "header.h"
 void drawmodBoard(int boardmatrix[ROWS][COLUMNS], int column, int player){
     int k, j;
-    // Check if the board should be modified or just printed
     if (column != -1 && player != -1){
         for (k = ROWS - 1; k >= 0; k--){
             if (boardmatrix[k][column] == 0){
@@ -10,7 +9,6 @@ void drawmodBoard(int boardmatrix[ROWS][COLUMNS], int column, int player){
             }
         }
     }
-    // Draw the board: now with color
     for (k = 0; k <= ROWS - 1; k++){
         printf("\n");
         for (j = 0; j <= COLUMNS - 1; j++){
@@ -18,12 +16,16 @@ void drawmodBoard(int boardmatrix[ROWS][COLUMNS], int column, int player){
                 case 0:
                     printf("\033[0m");
                     printf("| |");
+                    break;
                 case 1:
                     printf("\033[0;31m");
                     printf("|O|");
+                    break;
                 case 2:
                     printf("\033[0;33m");
                     printf("|O|");
+                    break;
+                default: break;
             }
         }
     }
