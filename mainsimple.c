c2ffa12377faf406f20ec55cc9e36794ab6d6849
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-09 01:19:45 -0700

    File IO for demos is back, no structs

--- mainsimple.c
+++ mainsimple.c
@@ -5,25 +5,27 @@ int main(void){
     int location = -1;
     int turncounter = 0;
     int valid = -1;
-	int boardmatrix[ROWS][COLUMNS] = {
-		{0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0},
-		{0,0,0,0,0,0,0},
-	};
+    int j, k;
+    int boardmatrix[ROWS][COLUMNS] = {
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0},
+    };
+    FILE* demo = fopen("allGames.txt", "a");
     // Adjustable win-messages
-	char redWins[] =	"\n The game has ended. Red player wins!\n";
-	char yellowWins[] =	"\n The game has ended. Yellow player wins!\n";
-	char tie[] =	    "\n The game has ended in a draw!\n";
-	resetdisplay();
-	drawmodBoard(boardmatrix, -1, -1);
+    char redWins[] =	"\n The game has ended. Red player wins!\n";
+    char yellowWins[] =	"\n The game has ended. Yellow player wins!\n";
+    char tie[] =	    "\n The game has ended in a draw!\n";
+    resetdisplay();
+    drawmodBoard(boardmatrix, -1, -1);
     // Now following the flow chart diagram
     while (running == 1){
         if (testforwin(boardmatrix) != 0){
-			break;
-		}
+            break;
+        }
         while (1){
             location = promptUser(player);
             valid = checkValidity(boardmatrix, location);
@@ -37,35 +39,52 @@ int main(void){
                 printf("\033[0m");
             }
         }
-		resetdisplay();
+        resetdisplay();
         drawmodBoard(boardmatrix, location, player);
-		turncounter++;
+        turncounter++;
         // Switch player using a switch statement:
-		switch (player){
-			case 1: 
-				player = 2;
-				break;
-			case 2: 
-				player = 1;
-				break;
+        switch (player){
+            case 1: 
+                player = 2;
+                break;
+            case 2: 
+                player = 1;
+                break;
             default:
                 player = 1;
-		}
+        }
     }
     // This section runs once at the very end of the game:
-	resetdisplay();
-	drawmodBoard(boardmatrix, -1, -1);
-	switch (testforwin(boardmatrix)){
-		case 1: 
-			printf("%s", redWins);
-			break;
-		case 2: 
-			printf("%s", yellowWins);
-			break;
-		case 3:
-			printf("%s", tie);
+    resetdisplay();
+    drawmodBoard(boardmatrix, -1, -1);
+    switch (testforwin(boardmatrix)){
+        case 1: 
+            printf("%s", redWins);
             break;
-	}
-	printf("\n");
+        case 2: 
+            printf("%s", yellowWins);
+            break;
+        case 3:
+            printf("%s", tie);
+            break;
+    }
+    printf("\n");
+    // Perform FileIO using a similar function to drawmodboard.
+        for (j = 0; j <= ROWS - 1; j++){
+            fprintf(demo, "\n ");
+            for (k = 0; k <= COLUMNS - 1; k++){
+                switch (boardmatrix[j][k]){
+                    case 0:
+                        fprintf(demo, "| |");
+                        break;
+                    case 1:
+                        fprintf(demo, "|%c|", 'X');
+                        break;
+                    case 2:
+                        fprintf(demo, "|%c|", 'O');
+                        break;
+                }
+            }
+    }
     return 0;
 }
