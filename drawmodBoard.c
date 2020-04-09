c075aebfb77efbe38a320dbac61f116a72f35e69
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-09 01:03:39 -0700

    Update drawmodBoard.c

--- drawmodBoard.c
+++ drawmodBoard.c
@@ -14,16 +14,15 @@ void drawmodBoard(int boardmatrix[ROWS][COLUMNS], int column, int player){
         for (j = 0; j <= COLUMNS - 1; j++){
             switch (boardmatrix[j][k]){
                 case 0:
-                    printf("\033[0m");
-                    printf(" ");
+                    printf("   ");
                     break;
                 case 1:
                     printf("\033[0;31m");
-                    printf("[O]");
+                    printf("|O|");
                     break;
                 case 2:
                     printf("\033[0;33m");
-                    printf("[O]");
+                    printf("|O|");
                     break;
             }
         }
