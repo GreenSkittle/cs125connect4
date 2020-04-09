aabf024bcbb45e57614d6b42445b8fc107605b7a
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-09 01:08:05 -0700

    typo; comments

--- drawmodBoard.c
+++ drawmodBoard.c
@@ -1,18 +1,21 @@
 #include "header.h"
 void drawmodBoard(int boardmatrix[ROWS][COLUMNS], int column, int player){
     int k, j;
+    // Does board need modifying or just printing?
     if (column != -1 && player != -1){
         for (k = ROWS - 1; k >= 0; k--){
             if (boardmatrix[k][column] == 0){
-                boardmatrix[k][column] = player;
+                // Simulate a piece for that player falling into place.
+                boardmatrix[k][column] = player; 
                 break;
             }
         }
     }
+    // Print in color each element
     for (k = 0; k <= ROWS - 1; k++){
         printf("\n ");
         for (j = 0; j <= COLUMNS - 1; j++){
-            switch (boardmatrix[k][J]){
+            switch (boardmatrix[k][j]){
                 case 0:
                     printf("| |");
                     break;
