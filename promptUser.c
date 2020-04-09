edad8f75e12c3ac18bdfbbbb4938b28a78a04688
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-09 00:25:26 -0700

    fixed the color for player 2 and formatting adjustments

--- promptUser.c
+++ promptUser.c
@@ -1,37 +1,37 @@
 #include "header.h"
 int promptUser(int player){
-	char string[255];
-	char choice;
-	int location;
-	int quickcheck;
-	switch (player){
-		case 1:
-			printf("\033[1;31m");
-			printf("\nPlayer 1: ");
-			printf("\033[0m");
-			printf("Pick a spot A-%c:\n", COLUMNS + 64);
-			quickcheck = scanf("\n%s", string);
-			choice = string[0]; // Take the first element of the string the user inputs
-			break;
-		case 2:
-			printf("\033[1;31m");
-			printf("\nPlayer 2: ");
-			printf("\033[0m");
-			printf("Pick a spot A-%c:\n", COLUMNS + 64);
-			quickcheck = scanf("\n%s", string);
-			choice = string[0]; // Take the first element of the string the user inputs
-			break;
-		default: break;
-	}
+    char string[255];
+    char choice;
+    int location;
+    int quickcheck;
+    switch (player){
+        case 1:
+            printf("\033[1;31m");
+            printf("\nPlayer 1: ");
+            printf("\033[0m");
+            printf("Pick a spot A-%c:\n", COLUMNS + 64);
+            quickcheck = scanf("\n%s", string);
+            choice = string[0]; // Take the first element of the string the user inputs
+            break;
+        case 2:
+            printf("\033[1;33m");
+            printf("\nPlayer 2: ");
+            printf("\033[0m");
+            printf("Pick a spot A-%c:\n", COLUMNS + 64);
+            quickcheck = scanf("\n%s", string);
+            choice = string[0]; // Take the first element of the string the user inputs
+            break;
+        default: break;
+    }
     // Map ASCII to a number for capitals or lowercase
-	if(choice >= 65 && choice <= 90 && quickcheck == 1){
-		location = choice - 65;
-	}
-	else if(choice >= 97 && choice <= 122 && quickcheck == 1){
-		location = choice - 97;
-	}
-	else {
-		location = -1;
-	}
-	return location;
+    if(choice >= 65 && choice <= 90 && quickcheck == 1){
+        location = choice - 65;
+    }
+    else if(choice >= 97 && choice <= 122 && quickcheck == 1){
+        location = choice - 97;
+    }
+    else {
+        location = -1;
+    }
+    return location;
 }
