7e059e1ca131d44b9098d8a1dbfdce4d39befe29
Bradyn Braithwaite <bpbraithwaite@gmail.com>
2020-04-09 00:20:58 -0700

    Update drawmodBoard.c
    comments and colors

--- drawmodBoard.c
+++ drawmodBoard.c
@@ -2,11 +2,9 @@
 void drawmodBoard(int boardmatrix[ROWS][COLUMNS], int column, int player){
     int k, j;
     if (column != -1 && player != -1){
-        // Check if we need to update the board
         for (k = ROWS - 1; k >= 0; k--){
             if (boardmatrix[k][column] == 0){
                 boardmatrix[k][column] = player;
-                // Simulates a piece falling into place
                 break;
             }
         }
@@ -14,8 +12,17 @@ void drawmodBoard(int boardmatrix[ROWS][COLUMNS], int column, int player){
     for (k = 0; k <= ROWS - 1; k++){
         printf("\n");
         for (j = 0; j <= COLUMNS - 1; j++){
-            printf(" %d", boardmatrix[k][j]);
-            // Print out each element in the array
+            switch (boardmatrix[j][k]){
+                case 0:
+                    printf("\033[0m");
+                    printf("| |");
+                case 1:
+                    printf("\033[0;31m");
+                    printf("|O|");
+                case 2:
+                    printf("\033[0;33m");
+                    printf("|O|");
+            }
         }
     }
     printf("\n");
